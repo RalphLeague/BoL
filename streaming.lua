8413d938470fa02b64f8f14b224753c31264f26e
RalphLeague <ralphlolbol@gmail.com>
2015-07-19 16:45:50 -0400

    rup

--- streaming.lua
+++ streaming.lua
@@ -1,12 +1,14 @@
-local XX, w, h1, h2 = false, (WINDOW_W*0.4), (WINDOW_H*.32), (WINDOW_H*.8)
+local w, h1, h2 = (WINDOW_W*0.4), (WINDOW_H*.32), (WINDOW_H*.8)
 print("Streaming Loaded")
 
-
 function OnTick()
-	XX = IsKeyDown(9) and not IsKeyDown(18)
-end
-function OnDraw()
-	if XX then
-		DrawLine(w, h1, w, h2, 200, ARGB(255,0,0,0))
+	if IsKeyDown(9) and not IsKeyDown(18) then
+		Draw = function() DrawLine(w, h1, w, h2, 200, ARGB(255,0,0,0)) end
+	else
+		Draw = function() end
 	end
 end
+
+function Draw()
+end
+AddDrawCallback(function() Draw() end)
