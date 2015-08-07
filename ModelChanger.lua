7aade37dd63f36621653156b1745386f9d3c316a
RalphLeague <ralphlolbol@gmail.com>
2015-08-07 14:05:39 -0400

    v1,31

--- ModelChanger.lua
+++ ModelChanger.lua
@@ -1,41 +1,6 @@
-_G.Model_Version = 1.3
-_G.Model_Autoupdate = true
+_G.Model_Version = 1.31
 timeran = os.clock()
-local script_downloadName = "Model Changer"
-local script_downloadHost = "raw.github.com"
-local script_downloadPath = "/RalphLeague/BoL/master/ModelChanger.lua" .. "?rand=" .. math.random(1, 10000)
-local script_downloadUrl = "https://" .. script_downloadHost .. script_downloadPath
-local script_filePath = SCRIPT_PATH .. GetCurrentEnv().FILE_NAME
-
-function script_Messager(message) print("<font color=\"#0000E5\">" .. script_downloadName .. ":</font> <font color=\"#FFFFFF\">" .. message) end
-
-if _G.Model_Autoupdate then
-	local script_webResult = GetWebResult(script_downloadHost, script_downloadPath)
-	if script_webResult then
-		local script_serverVersion = string.match(script_webResult, "%s*_G.Model_Version%s+=%s+%d+%.%d+")
-
-		if script_serverVersion then
-			script_serverVersion = tonumber(string.match(script_serverVersion or "", "%d+%.?%d*"))
-
-			if not script_serverVersion then
-				script_Messager("Please contact the developer of the script \"" .. script_downloadName .. "\", since the auto updater returned an invalid version.")
-				return
-			end
-
-			if _G.Model_Version < script_serverVersion then
-				script_Messager("New version available: " .. script_serverVersion)
-				script_Messager("Updating, please don't press F9")
-				DelayAction(function () DownloadFile(script_downloadUrl, script_filePath, function() script_Messager("Successfully updated the script, please reload and check the changelog!") end) end, 2)
-			else
-				script_Messager("You've got the latest version: " .. script_serverVersion)
-			end
-		else
-			script_Messager("Something went wrong, update the script manually!")
-		end
-	else
-		script_Messager("Error downloading server version!")
-	end
-end
+function Print(message) print("<font color=\"#0000E5\"> Model Changer:</font> <font color=\"#FFFFFF\">" .. message) end
 
 ModelNames = {
 	"      OFF",
@@ -103,6 +68,21 @@ Models = {
 	"redDragon",
 }
 function OnLoad()
+	local ToUpdate = {}
+    ToUpdate.Version = 1.31
+    ToUpdate.UseHttps = true
+    ToUpdate.Host = "raw.githubusercontent.com"
+    ToUpdate.VersionPath = "/RalphLeague/BoL/master/ModelChanger.version"
+    ToUpdate.ScriptPath =  "/RalphLeague/BoL/master/ModelChanger.lua"
+    ToUpdate.SavePath = SCRIPT_PATH.._ENV.FILE_NAME
+    ToUpdate.CallbackUpdate = function(NewVersion,OldVersion) Print("Updated to v"..NewVersion) end
+    ToUpdate.CallbackNoUpdate = function(OldVersion) Print("No Updates Found") end
+    ToUpdate.CallbackNewVersion = function(NewVersion) Print("New Version found ("..NewVersion.."). Please wait until its downloaded") end
+    ToUpdate.CallbackError = function(NewVersion) Print("Error while Downloading. Please try again.") end
+   
+	MCScriptUpdater(ToUpdate.Version,ToUpdate.UseHttps, ToUpdate.Host, ToUpdate.VersionPath, ToUpdate.ScriptPath, ToUpdate.SavePath, ToUpdate.CallbackUpdate,ToUpdate.CallbackNoUpdate, ToUpdate.CallbackNewVersion,ToUpdate.CallbackError)
+	Print(" Version "..ToUpdate.Version.." Loaded")
+	
 	Menu()
 	ModelChosen = 0
 	Menu.skins = false
@@ -143,7 +123,7 @@ function Menu()
 		Menu:addParam("model", "Change Model", SCRIPT_PARAM_LIST, 1, ModelNames)
 		
 		Menu:addParam("info4","", SCRIPT_PARAM_INFO, "")
-		Menu:addParam("use", "Use Spells", SCRIPT_PARAM_ONOFF, true)  
+		Menu:addParam("use", "Use Spells", SCRIPT_PARAM_ONOFF, false)  
 		Menu:addParam("CastQ", "Cast Q", SCRIPT_PARAM_ONKEYDOWN, false, GetKey('Q')) 
 		Menu:addParam("CastW", "Cast W", SCRIPT_PARAM_ONKEYDOWN, false, GetKey('W')) 
 		Menu:addParam("CastE", "Cast E", SCRIPT_PARAM_ONKEYDOWN, false, GetKey('E')) 
@@ -175,15 +155,22 @@ function OnTick()
 		end
 	end
 end
+--00 05 00 00 40 1A C0 78 78 78 1A 53 C1 C1 C1 C1 52 61 6D 6D 75 73 50 42 00 4C 6F 63 61 74 69 6F 08 00 00 00 0F 00 00 00 00 80 3F 83 
 function MakeModel(modelName)
-   local p = CLoLPacket(0x0055)
-	p.vTable = 15718744
+   local p = CLoLPacket(182)
+	p.vTable = 14784720
     p:EncodeF(player.networkID)
-	p:Encode1(0xE3)
-	p:Encode1(0xE3)
-	p:Encode1(0xE3)
-	p:Encode1(0xE3)
-	p:Encode1(0x6A)
+	p:Encode1(0x1A)
+	p:Encode1(0xC0)
+	p:Encode1(0x78)
+	p:Encode1(0x78)
+	p:Encode1(0x78)
+	p:Encode1(0x1A)
+	p:Encode1(0x53)
+	p:Encode1(0xC1)
+	p:Encode1(0xC1)
+	p:Encode1(0xC1)
+	p:Encode1(0xC1)
 	local count = 0
 	for c in modelName:gmatch'.' do
 	  p:Encode1(string.byte(c))
@@ -192,12 +179,7 @@ function MakeModel(modelName)
 	for i = 1, 16 - count do
 		p:Encode1(0x00)
 	end
-	--p:Encode1(0x47)
-	--p:Encode1(0x6E)
-	--p:Encode1(0x61)
-	--p:Encode1(0x72)
-	
-	p:Encode1(0x07)
+	p:Encode1(0x08)
 	p:Encode1(0x00)
 	p:Encode1(0x00)
 	p:Encode1(0x00)
@@ -205,9 +187,201 @@ function MakeModel(modelName)
 	p:Encode1(0x00)
 	p:Encode1(0x00)
 	p:Encode1(0x00)
+	p:Encode1(0x00)
 	p:Hide()
---	local file = io.open(SCRIPT_PATH .. "skinchanger.txt", "a")
---	file:write(DumpPacketData(p))
---	file:close()
+	--local file = io.open(SCRIPT_PATH .. "skinchanger.txt", "a")
+	--file:write(DumpPacketData(p))
+	--file:close()
     RecvPacket(p)
+end
+
+class "MCScriptUpdater"
+function MCScriptUpdater:__init(LocalVersion,UseHttps, Host, VersionPath, ScriptPath, SavePath, CallbackUpdate, CallbackNoUpdate, CallbackNewVersion,CallbackError)
+    self.LocalVersion = LocalVersion
+    self.Host = Host
+    self.VersionPath = '/BoL/TCPUpdater/GetScript'..(UseHttps and '5' or '6')..'.php?script='..self:Base64Encode(self.Host..VersionPath)..'&rand='..math.random(99999999)
+    self.ScriptPath = '/BoL/TCPUpdater/GetScript'..(UseHttps and '5' or '6')..'.php?script='..self:Base64Encode(self.Host..ScriptPath)..'&rand='..math.random(99999999)
+    self.SavePath = SavePath
+    self.CallbackUpdate = CallbackUpdate
+    self.CallbackNoUpdate = CallbackNoUpdate
+    self.CallbackNewVersion = CallbackNewVersion
+    self.CallbackError = CallbackError
+    --AddDrawCallback(function() self:OnDraw() end)
+    self:CreateSocket(self.VersionPath)
+    self.DownloadStatus = 'Connect to Server for VersionInfo'
+    AddTickCallback(function() self:GetOnlineVersion() end)
+end
+
+function MCScriptUpdater:print(str)
+    print('<font color="#FFFFFF">'..os.clock()..': '..str)
+end
+
+function MCScriptUpdater:OnDraw()
+    if self.DownloadStatus ~= 'Downloading Script (100%)' and self.DownloadStatus ~= 'Downloading VersionInfo (100%)'then
+        DrawText('Download Status: '..(self.DownloadStatus or 'Unknown'),50,10,50,ARGB(0xFF,0xFF,0xFF,0xFF))
+    end
+end
+
+function MCScriptUpdater:CreateSocket(url)
+    if not self.LuaSocket then
+        self.LuaSocket = require("socket")
+    else
+        self.Socket:close()
+        self.Socket = nil
+        self.Size = nil
+        self.RecvStarted = false
+    end
+    self.LuaSocket = require("socket")
+    self.Socket = self.LuaSocket.tcp()
+    self.Socket:settimeout(0, 'b')
+    self.Socket:settimeout(99999999, 't')
+    self.Socket:connect('sx-bol.eu', 80)
+    self.Url = url
+    self.Started = false
+    self.LastPrint = ""
+    self.File = ""
+end
+
+function MCScriptUpdater:Base64Encode(data)
+    local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    return ((data:gsub('.', function(x)
+        local r,b='',x:byte()
+        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c=0
+        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+        return b:sub(c+1,c+1)
+    end)..({ '', '==', '=' })[#data%3+1])
+end
+
+function MCScriptUpdater:GetOnlineVersion()
+    if self.GotScriptVersion then return end
+
+    self.Receive, self.Status, self.Snipped = self.Socket:receive(1024)
+    if self.Status == 'timeout' and not self.Started then
+        self.Started = true
+        self.Socket:send("GET "..self.Url.." HTTP/1.1\r\nHost: sx-bol.eu\r\n\r\n")
+    end
+    if (self.Receive or (#self.Snipped > 0)) and not self.RecvStarted then
+        self.RecvStarted = true
+        self.DownloadStatus = 'Downloading VersionInfo (0%)'
+    end
+
+    self.File = self.File .. (self.Receive or self.Snipped)
+    if self.File:find('</s'..'ize>') then
+        if not self.Size then
+            self.Size = tonumber(self.File:sub(self.File:find('<si'..'ze>')+6,self.File:find('</si'..'ze>')-1))
+        end
+        if self.File:find('<scr'..'ipt>') then
+            local _,ScriptFind = self.File:find('<scr'..'ipt>')
+            local ScriptEnd = self.File:find('</scr'..'ipt>')
+            if ScriptEnd then ScriptEnd = ScriptEnd - 1 end
+            local DownloadedSize = self.File:sub(ScriptFind+1,ScriptEnd or -1):len()
+            self.DownloadStatus = 'Downloading VersionInfo ('..math.round(100/self.Size*DownloadedSize,2)..'%)'
+        end
+    end
+    if self.File:find('</scr'..'ipt>') then
+        self.DownloadStatus = 'Downloading VersionInfo (100%)'
+        local a,b = self.File:find('\r\n\r\n')
+        self.File = self.File:sub(a,-1)
+        self.NewFile = ''
+        for line,content in ipairs(self.File:split('\n')) do
+            if content:len() > 5 then
+                self.NewFile = self.NewFile .. content
+            end
+        end
+        local HeaderEnd, ContentStart = self.File:find('<scr'..'ipt>')
+        local ContentEnd, _ = self.File:find('</sc'..'ript>')
+        if not ContentStart or not ContentEnd then
+            if self.CallbackError and type(self.CallbackError) == 'function' then
+                self.CallbackError()
+            end
+        else
+            self.OnlineVersion = (Base64Decode(self.File:sub(ContentStart + 1,ContentEnd-1)))
+            self.OnlineVersion = tonumber(self.OnlineVersion)
+            if self.OnlineVersion > self.LocalVersion then
+                if self.CallbackNewVersion and type(self.CallbackNewVersion) == 'function' then
+                    self.CallbackNewVersion(self.OnlineVersion,self.LocalVersion)
+                end
+                self:CreateSocket(self.ScriptPath)
+                self.DownloadStatus = 'Connect to Server for ScriptDownload'
+                AddTickCallback(function() self:DownloadUpdate() end)
+            else
+                if self.CallbackNoUpdate and type(self.CallbackNoUpdate) == 'function' then
+                    self.CallbackNoUpdate(self.LocalVersion)
+                end
+            end
+        end
+        self.GotScriptVersion = true
+    end
+end
+
+function MCScriptUpdater:DownloadUpdate()
+    if self.GotMCScriptUpdater then return end
+    self.Receive, self.Status, self.Snipped = self.Socket:receive(1024)
+    if self.Status == 'timeout' and not self.Started then
+        self.Started = true
+        self.Socket:send("GET "..self.Url.." HTTP/1.1\r\nHost: sx-bol.eu\r\n\r\n")
+    end
+    if (self.Receive or (#self.Snipped > 0)) and not self.RecvStarted then
+        self.RecvStarted = true
+        self.DownloadStatus = 'Downloading Script (0%)'
+    end
+
+    self.File = self.File .. (self.Receive or self.Snipped)
+    if self.File:find('</si'..'ze>') then
+        if not self.Size then
+            self.Size = tonumber(self.File:sub(self.File:find('<si'..'ze>')+6,self.File:find('</si'..'ze>')-1))
+        end
+        if self.File:find('<scr'..'ipt>') then
+            local _,ScriptFind = self.File:find('<scr'..'ipt>')
+            local ScriptEnd = self.File:find('</scr'..'ipt>')
+            if ScriptEnd then ScriptEnd = ScriptEnd - 1 end
+            local DownloadedSize = self.File:sub(ScriptFind+1,ScriptEnd or -1):len()
+            self.DownloadStatus = 'Downloading Script ('..math.round(100/self.Size*DownloadedSize,2)..'%)'
+        end
+    end
+    if self.File:find('</scr'..'ipt>') then
+        self.DownloadStatus = 'Downloading Script (100%)'
+        local a,b = self.File:find('\r\n\r\n')
+        self.File = self.File:sub(a,-1)
+        self.NewFile = ''
+        for line,content in ipairs(self.File:split('\n')) do
+            if content:len() > 5 then
+                self.NewFile = self.NewFile .. content
+            end
+        end
+        local HeaderEnd, ContentStart = self.NewFile:find('<sc'..'ript>')
+        local ContentEnd, _ = self.NewFile:find('</scr'..'ipt>')
+        if not ContentStart or not ContentEnd then
+            if self.CallbackError and type(self.CallbackError) == 'function' then
+                self.CallbackError()
+            end
+        else
+            local newf = self.NewFile:sub(ContentStart+1,ContentEnd-1)
+            local newf = newf:gsub('\r','')
+            if newf:len() ~= self.Size then
+                if self.CallbackError and type(self.CallbackError) == 'function' then
+                    self.CallbackError()
+                end
+                return
+            end
+            local newf = Base64Decode(newf)
+            if type(load(newf)) ~= 'function' then
+                if self.CallbackError and type(self.CallbackError) == 'function' then
+                    self.CallbackError()
+                end
+            else
+                local f = io.open(self.SavePath,"w+b")
+                f:write(newf)
+                f:close()
+                if self.CallbackUpdate and type(self.CallbackUpdate) == 'function' then
+                    self.CallbackUpdate(self.OnlineVersion,self.LocalVersion)
+                end
+            end
+        end
+        self.GotMCScriptUpdater = true
+    end
 end
