a91c8e22f40974502b5007a0025a422211c1bca5
RalphLeague <ralphlolbol@gmail.com>
2017-04-20 12:10:50 -0400

    v708

--- MachineLib.lua
+++ MachineLib.lua
@@ -2,33 +2,33 @@
 
 class 'MachineLib'
 function MachineLib:__init()
-	self.version = 707.1
+	self.version = 708
 	
-	self.lolPatch = GetGameVersion and GetGameVersion():sub(1,3) == "7.7" and 1 or 2
-	self.notPresentPatch = GetGameVersion and GetGameVersion():sub(1,3) ~= "7.6" and GetGameVersion():sub(1,3) ~= "7.7"
+	self.lolPatch = GetGameVersion and GetGameVersion():sub(1,3) == "7.8" and 1 or 2
+	self.notPresentPatch = GetGameVersion and GetGameVersion():sub(1,3) ~= "7.7" and GetGameVersion():sub(1,3) ~= "7.8"
 	self.cn = self.notPresentPatch
 	
 	--Send--
-		self.moveHeader = self.lolPatch == 1 and 43 or 339
-		self.spellHeader = self.lolPatch == 1 and 127 or 88
+		self.moveHeader = self.lolPatch == 1 and 312 or 43
+		self.spellHeader = self.lolPatch == 1 and 156 or 127
 		
 		--cspell2
-		self.cspell2Header = self.lolPatch == 1 and 287 or 139
-		self.cspell2Pos = self.lolPatch == 1 and 6 or 13
-		self.cspell2Byte = self.lolPatch == 1 and 0x42 or 0x47 
+		self.cspell2Header = self.lolPatch == 1 and 57 or 287
+		self.cspell2Pos = self.lolPatch == 1 and 6 or 6
+		self.cspell2Byte = self.lolPatch == 1 and 0x42 or 0x42 
 	
 	--Rsv--
-		self.goldHeader = self.lolPatch == 1 and 11 or 137
+		self.goldHeader = self.lolPatch == 1 and 34 or 11
 		
 		--Buy
-		self.bHeader = self.lolPatch == 1 and 0x46 or 157
-		self.bPos = self.lolPatch == 1 and 19 or 17
-		self.botrk = self.lolPatch == 1 and 212 or 243
-		self.IE = self.lolPatch == 1 and 171 or 6
-		self.Trinity = self.lolPatch == 1 and 206 or 128
+		self.bHeader = self.lolPatch == 1 and 52 or 0x46
+		self.bPos = self.lolPatch == 1 and 14 or 19
+		self.botrk = self.lolPatch == 1 and 202 or 212
+		self.IE = self.lolPatch == 1 and 247 or 171
+		self.Trinity = self.lolPatch == 1 and 125 or 206
 	
 		--Recall
-		self.recallHeader = self.lolPatch == 1 and 333 or 10
+		self.recallHeader = self.lolPatch == 1 and 43 or 333
 		self.recallPos1 = self.lolPatch == 1 and 6 or 80 --id
 		self.recallPos2 = self.lolPatch == 1 and  35 or 56
 	
